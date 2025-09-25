b1012c13608475d61c43755c0cc4e17a2322cc7a
Mostafa Said <m.ssaid356@gmail.com>
2025-09-25 22:29:56 +0300

    edit file program

--- Booksy/Program.cs
+++ Booksy/Program.cs
@@ -1,6 +1,23 @@
 using Booksy.Extentions;
+using Booksy.Utility.DBInitializer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+using Stripe;
 
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                      });
+
+});
 
 // Add services to the container.
 
@@ -13,18 +30,63 @@ builder.Services.AddEndpointsApiExplorer();
 //builder.Services.AddSwaggerGenJwtAuth();
 builder.Services.AddOpenApi();
 builder.Services.AddCustomJwtAuth(builder.Configuration);
+builder.Services.AddSwaggerGen();
+//builder.Services.AddOpenApi();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IDBInitializer, DBInitializer>();
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(option =>
+{
+    option.Password.RequiredLength = 6;
+    option.Password.RequireNonAlphanumeric = false;
+    option.User.RequireUniqueEmail = true;
+})
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Identity/Account/Login";
+    options.AccessDeniedPath = "/Customer/Home/NotFoundPage";
+});
+
+builder.Services.AddTransient<IEmailSender, EmailSender>();
+
+builder.Services.AddScoped<IRepository<Category>, Repository<Category>>();
+builder.Services.AddScoped<IRepository<Brand>, Repository<Brand>>();
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IRepository<UserOTP>, Repository<UserOTP>>();
+builder.Services.AddScoped<IDBInitializer, DBInitializer>();
+builder.Services.AddScoped<IRepository<Cart>, Repository<Cart>>();
+builder.Services.AddScoped<IRepository<Promotion>, Repository<Promotion>>();
+builder.Services.AddScoped<IRepository<Order>, Repository<Order>>();
+builder.Services.AddScoped<IOrderItemRepository, OrderItemRepository>();
+
+
+builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
+StripeConfiguration.ApiKey = builder.Configuration["Stripe:SecretKey"];
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapScalarApiReference();
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors(MyAllowSpecificOrigins);
+app.UseAuthentication();
 app.UseAuthorization();
+//app.UseSession();
+
+var scope = app.Services.CreateScope();
+var service = scope.ServiceProvider.GetService<IDBInitializer>();
+service.Initialize();
 
 app.MapControllers();
 
