3a46165e7d71157676c4a461369961e694e31c1f
Mostafa Said <m.ssaid356@gmail.com>
2025-09-27 23:06:44 +0300

    Last Version

--- Booksy/Program.cs
+++ Booksy/Program.cs
@@ -38,11 +38,7 @@ var app = builder.Build();
 // Enable CORS
 app.UseCustomCors();
 // Cookie settings
-services.ConfigureApplicationCookie(options =>
-{
-    options.LoginPath = "/Identity/Account/Login";
-    options.AccessDeniedPath = "/Customer/Home/NotFoundPage";
-});
+
 // Enable Swagger (for dev environment)
 if (app.Environment.IsDevelopment())
 {
